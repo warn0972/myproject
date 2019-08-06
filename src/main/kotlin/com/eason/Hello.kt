db55fc9e7026e84f38c493d5a55c7eec81787cf5
warn0972 <warn_error@yahoo.com.tw>
2019-08-06 10:27:44 +0800

    Initial commit

---
+++ src/main/kotlin/com/eason/Hello.kt
@@ -0,0 +1,14 @@
+package com.eason
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+//        Human().hello()
+        val h = Human()
+        h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello Kotlin")
+    }
+}
